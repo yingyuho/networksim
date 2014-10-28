6f77fad5b052d13c23973336e031412b0bbb6498
Ying-Yu Ho <heinsolid@gmail.com>
2014-10-28 04:28:01 -0700

    copy and modify simpy.Store for buffer use

---
+++ src/simpy_ext.py
@@ -0,0 +1,47 @@
+from __future__ import division, print_function
+from collections import deque
+
+from simpy.resources.store import Store
+
+class SizedStore(Store):
+    """Models the production and consumption of concrete Python objects of 
+    variable sizes.
+
+    Items put into the store can be of any type.  By default, they are put and
+    retrieved from the store in a first-in first-out order.
+
+    The *env* parameter is the :class:`~simpy.core.Environment` instance the
+    container is bound to.
+
+    The *capacity* defines the size of the Store and must be a positive number
+    (> 0). By default, a Store is of unlimited size. A :exc:`ValueError` is
+    raised if the value is negative.
+
+    The *sizefunc* returns the size of an item when applied on it. The return
+    value must be a non-negative integer.
+
+    """
+    def __init__(self, env, capacity=float('inf'), sizefunc=1):
+        super(SizedStore, self).__init__(env, capacity)
+        self.items = deque()
+        """List of the items within the store."""
+        if not hasattr(sizefunc, '__call__'):
+            sizefunc = (lambda _: sizefunc)
+        self._sizefunc = sizefunc
+        self._level = 0
+
+    def _do_put(self, event):
+        item = event.item
+        size = self._sizefunc(item)
+        if (not isinstance(size, (int, long))) or (size < 0):
+            raise ValueError('Size of item must be a non-negative integer.')
+        if (self._level + size) <= self._capacity:
+            self._level += size
+            self.items.append(item)
+            event.succeed()
+
+    def _do_get(self, event):
+        if self.items:
+            item = self.items.popleft()
+            self._level -= self._sizefunc(item)
+            event.succeed(item)
