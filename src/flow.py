91c93b3ecf3679b9b7ad6885a424f40e398be60e
Emily Shih <eshih@caltech.edu>
2014-11-03 04:19:19 -0800

    Added method to enable parsing of test case text files. Added very basic Flow class file.

---
+++ src/flow.py
@@ -0,0 +1,8 @@
+class Flow(object):
+	def __init__(self, env, flow_id, src_id, dest_id, data_mb, start_s):
+		self.env = env
+		self.id = flow_id
+		self.src = src_id
+		self.dest = dest_id
+		self.data = data_mb
+		self.start = start_s
--- src/network.py
+++ src/network.py
@@ -1,22 +1,59 @@
 from __future__ import division, print_function
 import simpy
+import os
 from device import Host, Link, Router
 from packet import DataPacket
+from flow import Flow
 
 class Network(object):
+
     """docstring for Network"""
-    def __init__(self, env=None):
+
+    def parse_network(self,filename):
+        curpath = os.path.dirname(__file__)
+        filepath = os.path.abspath(os.path.join(curpath, "..", "testcases", filename))
+        f = open(filepath,'r')
+        sect_idx = 0
+        for line in f:
+            if line[0] == '-':
+                sect_idx += 1
+            elif sect_idx == 0:
+                h = Host(self.env, line[0:2])
+                self.hosts.append(h)
+            elif sect_idx == 1:
+                r = Router(self.env, line[0:2])
+                self.routers.append(r)
+            elif sect_idx == 2:
+                arr = line.split()
+                l = Link(self.env, arr[0], arr[3], arr[4], arr[5])
+                # Discuss changing constructor for Links, getting rid of the "attach"
+                # method for device. Unless these attachments are dynamic, there's no
+                # point in making a method to attach them as we go along...
+                self.links.append(l)
+            else:
+                arr = line.split()
+                f = Flow(self.env, arr[0], arr[1], arr[2], arr[3], arr[4])
+                self.flows.append(l)
+                
+
+    def __init__(self, env, filename):
         super(Network, self).__init__()
+
+        self.hosts = []
+        self.routers = []
+        self.links = []
+        self.flows = []
 
         if env is None:
             env = simpy.Environment()
         self.env = env
 
+        self.parse_network(filename)
+
+
     def run(until=None):
         return self.env.run(until=until)
-
-def main():
-    pass
+    
 
 if __name__ == '__main__':
-    main()
+    tc0 = Network(None, 'tc0.txt')
