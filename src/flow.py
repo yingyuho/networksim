35bc98bfbb0ddf6313cfde930921d16eaa0763d5
Emily Shih <eshih@caltech.edu>
2014-11-26 21:55:32 -0800

    Removed _cwnd from FAST because it is now in Flow

--- src/flow.py
+++ src/flow.py
@@ -301,8 +301,8 @@ class FASTTCP(Flow):
         self.avgRTT
 
         # window size
-        self.window_size = 1
-        self.allowance = simpy.Container(env, init=self.window_size)
+        self._cwnd = 1
+        self.allowance = simpy.Container(env, init=self._cwnd)
         self.debt = 0
 
         # calculated queue delay
@@ -367,16 +367,14 @@ class FASTTCP(Flow):
             self.RTT = self.env.now - pktt.timestamp
             if self.RTT < self.baseRTT:
                 self.baseRTT = self.RTT
-            weight = min(3/self.window_size, 1/4)
+            weight = min(3/self._cwnd, 1/4)
             self.avgRTT = (1 - weight * self.avgRTT + weight * self.RTT)
             self.queue_delay = self.avgRTT - self.baseRTT
 
             # Window control
             gamma = .5
             alpha = 1
-            old_size = self.window_size
-            self.window_size = min(2 * self.window_size, 
-                (1 - gamma)*self.window_size + gamma(self.baseRTT/self.RTT * self.window_size + alpha))
-            self.debt += old_size - self.window_size
-            inc_allowance(self, self.window_size)
+            wind_size = min(2 * self._cwnd, 
+                (1 - gamma)*self._cwnd + gamma(self.baseRTT/self.RTT * self._cwnd + alpha))
+            cwnd(self, wind_size)
 
